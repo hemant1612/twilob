c2a960109d52912082aa8b428cf428e3ae7e2881
Hemant <hemantsingh1612@gmail.com>
2018-11-16 23:40:25 +0530

    added model

--- l.py
+++ l.py
@@ -1,39 +1,34 @@
 from textblob import TextBlob
 from textblob.classifiers import NaiveBayesClassifier
 import csv
-spamReader = csv.reader(open('train.csv', newline=''), delimiter='\t', quotechar='|')
+spamReader = csv.reader(open('train_amazon.csv', newline=''), delimiter='\t', quotechar='|')
 train=[]
 count=0
 for row in spamReader:
 	count+=1
-	if count==20000:
+	if count==3000:
 		break
-	#print((row[2],row[3]))
-	train.append((row[2],row[3]))
-mydict={'0':'negative','1': 'somewhat negative','2': 'neutral','3': 'somewhat positive','4' :'positive'}
+	if(len(row) == 2):
+	    train.append((row[0],row[1]))
+	else:
+	    print(row)
+    
+
+#	train.append((row[1],row[2]))
+mydict={'1':'negative','2': 'somewhat negative','3': 'neutral','4': 'somewhat positive','5' :'positive'}
 cl = NaiveBayesClassifier(train)
 
-
-
-
-
-
-
+cl.show_informative_features(30)
 
 print(mydict[cl.classify("I feel amazing!")])
-blob = TextBlob("The beer is good. But the hangover is horrible.", classifier=cl)
-for s in blob.sentences:
-     print(s)
-     print(mydict[s.classify()])
-print(mydict[cl.classify("I am nervous")])
-print(mydict[cl.classify("I hate him")])
-print(mydict[cl.classify("that was a boring and slow movie")])
-cl.show_informative_features(5)
+print(mydict[cl.classify("poor quality")])
 
-spamReader2 = csv.reader(open('testing.csv', newline=''), delimiter='\t', quotechar='|')
+
+
+spamReader2 = csv.reader(open('testing_amazon.csv', newline=''), delimiter='\t', quotechar='|')
 tra = []
 for row in spamReader2:
     count+=1
-    tra.append((row[2],row[3]))
+    tra.append((row[0],row[1]))
 
 print(cl.accuracy(tra))
